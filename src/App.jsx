d96de8ce319144fea4a2bacf1fd9035680d4a616
Heshan Gimhana <galgodageheshan@gmail.com>
2023-11-27 16:09:48 +0530

    moon

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ function App() {
     <div className="header-section w-[100%] relative">
       <img
         className="w-[100%]"
-        src="src/assets/images/bg-desktop-light.jpg"
+        src="assets/images/bg-desktop-light.jpg"
         alt="background"
       />
       <div
